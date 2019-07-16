4d267fe520f2a232fe0d70b0d57375ca4a473f2e
Antonio Garcia <agarcia347@student.rccd.edu>
2019-07-16 16:47:00 -0500

    Added Dog App

---
+++ 1-csharp/DogApp/DogApp/Dog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogApp
+{
+    class Dog
+    {
+        //private string name;
+        //private string owner;
+
+        //public string GetName()
+        //{
+        //    return name;
+        //}
+
+        //public string GetOwner()
+        //{
+        //    return owner;
+        //}
+
+        //public void SetOwner(string owner)
+        //{
+        //    if (owner == null)
+        //    {
+        //        throw new ArgumentNullException("Owner");
+        //    }
+        //    this.owner = owner;
+        //}
+
+        private string Name { get; set; }
+
+        private string _owner;
+
+        public string Owner
+        {
+            get { return _owner; }
+            set
+            {
+                if ( value == null)
+                {
+                    throw new ArgumentNullException("Owner");
+                }
+
+                _owner = value;
+            }
+        }
+
+        public string Identifier
+        {
+            get
+            {
+                // string interpolation syntax
+                return $"{Owner} - {Name}";
+            }
+        }
+
+        public Dog(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Bark!!!");
+        }
+    }
+}
---
+++ 1-csharp/DogApp/DogApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DogApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dog = new Dog("Scooby Doo");
+            dog.Owner = "Shaggy";
+            dog.Bark();
+            Console.WriteLine(dog.Identifier);
+        }
+    }
+}
